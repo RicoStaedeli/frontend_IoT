3e63b91da9ab838d44cda31481aef07259f5b668
Rico Städeli <rico@yabriga.ch>
2023-11-05 12:17:31 +0100

    add dummy request call

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
+# ###############################
+# HTML Endpoints
+# ###############################
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 @app.route('/sensors')
 def sensors():
-    return render_template('sensors.html')
+    api_url = "https://jsonplaceholder.typicode.com/posts/"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        sensordata = response.json()
+        sensordata = getSnesorData()
+        return render_template('sensors.html',actualValue=sensordata.get("value"),catWeight=sensordata.get("catweight"))
+    else:
+        return render_template('sensors.html',weightSensorValue='Fehler')
 
 @app.route('/weightsensor')
 def weightsensor():
     return render_template('WeightSensor.html')
 
-@app.route("/event/<action>")
-def event(action):
-    return {}, 200
+# ###############################
+# Helper Methods
+# ###############################
+def getSnesorData():
+    response = {
+        "sensorId": 564,
+        "value": 12.156,
+        "catweight": 3.12}
+    return response
---
+++ test.py
@@ -0,0 +1,6 @@
+response = {
+    "sensorId": 564,
+    "value": 12.156,
+    "catweight": 3.12}
+
+print(response)
